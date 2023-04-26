562d2c8be97ea161c749e9eac614e51ffcda037a
krishna koustub <krishnakoustub@gmail.com>
2023-04-26 12:37:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivy.app import App 
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class BoxLayoutExample(BoxLayout):
+    pass
+"""     
+ def __int__(self, **kwargs):
+      super().__inint__(**kwargs)
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        b3 = Button(text="C")
+
+
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3)
+"""
+class MainWidget(Widget):
+    pass
+
+class TheLabApp(App):
+    pass
+
+TheLabApp().run()
